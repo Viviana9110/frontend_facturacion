c80d3e622aebd367e573983011f772e0103714d7
Viviana9110 <v.londono.naranjo@gmail.com>
2026-04-06 18:02:03 -0500

    correccion modal

--- src/components/InvoicePreview.jsx
+++ src/components/InvoicePreview.jsx
@@ -39,12 +39,12 @@ export default function InvoicePreview({ invoice }) {
 
       {/* BOTÓN PDF */}
       <div className="flex justify-end mb-2">
-        <button
+        {/* <button
           onClick={downloadPDF}
           className="bg-red-600 hover:bg-red-700 text-white px-4 py-2 rounded-xl transition"
         >
           Descargar PDF
-        </button>
+        </button> */}
       </div>
 
       {/* CONTENEDOR VISUAL */}
